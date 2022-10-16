d3a03fcdeb591a65b99f12844abb5eb924c422d0
Danil <danil-8951@mail.ru>
2022-10-16 15:39:06 +0500

    Программа, которая показывает второе число трехзначной цифры

---
+++ Ex010/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число: ");
+string numberA = Console.ReadLine();
+int number = int.Parse(numberA);
+
+if(number >= 100 && number <= 999)
+{
+    int result = Math.Abs(number / 10 % 10); 
+    Console.WriteLine($"Вторая цифра трехзначного числа {result}");
+}
+else
+{
+    Console.WriteLine($"Данные не верные ");
+}
